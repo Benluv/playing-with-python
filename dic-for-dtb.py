67a2514fb8f18042795fe71449a0946012cbbf6a
Benito Luongo <luongov13@gmail.com>
2018-01-28 17:25:24 -0600

    revision one

---
+++ dic-for-dtb.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jan 28 15:51:10 2018
+
+@author: User Raptorjesus13
+"""
+
+
+print("this is just a randnom test")
+print()
+
+coolBeans = {'Director': 'pedro',
+             'Department': 'human resouces',
+             'Nickname': 'el chino',
+             'Hobbies': 'loves cooking'}
+
+name_and_department = (str(coolBeans['Director']) + ' works in the '
+                       + str(coolBeans['Department']))
+
+coolBeans['Company'] = 'Ricos Tacos Inc'
+
+#pedro does not find it funny
+del(coolBeans['Nickname'])
+
+print(coolBeans)
+
