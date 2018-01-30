1ce69b9484a54c78369ea37a230e50f821ea1dc6
Benralv <luongov13@gmail.com>
2018-01-29 18:58:04 -0600

    Final revision: Seems I testing for a log-linear function for the two models and compared it to the linear regression. Instead I just had to check for the log-likelihood. I had finally to compare which of the two models is better (which one best represents the data) with both the SSE (sum of squared errors) and for the log likelihood

--- sq-log_model.py
+++ sq-log_model.py
@@ -6,7 +6,7 @@ This is a temporary script file.
 """
 
 import numpy as np
-
+from scipy.stats import norm
 
 ##############--FUNCTION DECLARATIONS--################
 
@@ -19,19 +19,18 @@ def calculate_y_hat(val, n) :
         n = n - 1
     return y_hat_final
 
-# constructing a new array with all the values of y hat given the logarithmic
-# model
-def calculate_ln_y_hat(val, m) :
-    y_hat_final_ln = []
-    while m >= 0:
-        y_app = np.log(x1[4-m]*val[0] + x2[4-m]*val[1] + val[2])
-        y_hat_final_ln.append(y_app)
-        m = m - 1
-    return y_hat_final_ln
+# calculating the log-likelihood of the models
+def Log_Likelihood(val, std) :
+    L = []
+    for x in val :
+        error = norm.pdf(x, 0, std)
+        ln_error = np.log(error)
+        L = np.append(L, ln_error)
+    return (np.sum(L))
 
 # adding some space, trying to save some time
 def space() :
-    print();print();print()
+    print(" ");print(" ");print(' ')
     return
 
 # calculating the standard error of every single observation
@@ -51,7 +50,7 @@ def intro(name, val) :
     if val == 1 :
         print("This is " + name + " linear regression")
     else :
-        print("This is " + name + " Log-Linear regression")
+        print("This is " + name + " Log likelihood regression")
     print()
 #######################################################
 
@@ -63,85 +62,60 @@ x2 = np.array([4,3,6,5,2])
 y = np.array([50,53,67,70,63])
 
 # Function for Model 1 y-hat
-y_hat_1 = np.array([1.5, 5, 12])
+y_beta_1 = np.array([1.5, 5, 12])
 # Function for Model 2 y-hat
-y_hat_2 = np.array([2.0, 4, 10])
+y_beta_2 = np.array([2.0, 4, 10])
 
 # Gets the size of the array (5)
 c = x1.size - 1    
-
-print("using sum of squared errors as the objective function")    
+print(" ")
+print("We calculate the y hat of each model and the standard error.")
+print("We follow by calculating the sum of squared errors, the standard "
+      "deviation and the log likelihood. ")    
 
 #model 1
 
 intro("model 1", 1)
 
 # makes all the calculations respectively for model 1 (refer to functions)
-y_mod_1 = calculate_y_hat(y_hat_1, c)
+y_mod_1 = calculate_y_hat(y_beta_1, c)
 error_mod_1 = error(y_mod_1, y)
-sse4error_1 = error_mod_1**2
-sse_1 = np.sum(sse4error_1)
+sse_1 = np.sum(error_mod_1**2)
+std_1 = std(sse_1, c+1)
 
 # prints the data in an orderly fashion
 print("the y hat of model 1 is :") 
 print(y_mod_1)
 print("and the error is: ")
-print(str(error_mod_1) + " with standard error " + str(sse_1))
-print("the standard deviation is :" + str(std(sse_1, c+1)))
+print(str(error_mod_1) + " with the sum of squared errors: " + str(sse_1))
+print("the standard deviation is :" + str(std_1))
+print("the log likelihood is: " + str(Log_Likelihood(error_mod_1, std_1)))
 
 #model 2
 
 intro("model 2", 1)
 
 # makes all the calculations respectively for model 2 (refer to functions)
-y_mod_2 = calculate_y_hat(y_hat_2, c)
+y_mod_2 = calculate_y_hat(y_beta_2, c)
 error_mod_2 = error(y_mod_2, y)
-sse4error_2 = error_mod_2**2
-sse_2 = np.sum(sse4error_2)
+sse_2 = np.sum(error_mod_2**2)
+std_2 = std(sse_2, c+1)
 
 # prints the data in an orderly fashion
 print("the y hat of model 1 is :") 
 print(y_mod_2)
 print("and the error is: ")
-print(str(error_mod_2) + " with standard error " + str(sse_2))
-print("the standard deviation is :" + str(std(sse_2, c+1)))
+print(str(error_mod_2) + " with the sum of squared errors: " + str(sse_2))
+print("the standard deviation is :" + str(std_2))
+print("the log likelihood is: " + str(Log_Likelihood(error_mod_2, std_2)))
 
 space()
 
-print("using log likelihood as the objective function")
-
-#model 1
-
-intro("model 1", 2)
-
-# makes all the calculations respectively for model 1 (refer to functions)
-y_mod_1_ln = calculate_ln_y_hat(y_hat_1, c)
-error_mod_1_ln = error(y_mod_1_ln, y)
-sse4error_1_ln = error_mod_1_ln**2
-sse_1_ln = np.sum(sse4error_1_ln)
-
-# prints the data in an orderly fashion
-print("the y hat of log  model 1 is :") 
-print(y_mod_1_ln)
-print("and the error is: ")
-print(str(error_mod_1_ln) + " with standard error " + str(sse_1_ln))
-print("the standard deviation is :" + str(std(sse_1_ln, c+1)))
-
-intro("model 2", 100)
-
-# makes all the calculations respectively for model 2 (refer to functions)
-y_mod_2_ln = calculate_ln_y_hat(y_hat_2, c)
-error_mod_2_ln = error(y_mod_2_ln, y)
-sse4error_2_ln = error_mod_2_ln**2
-sse_2_ln = np.sum(sse4error_2_ln)
-
-# prints the data in an orderly fashion
-print("the y hat of log  model 2 is :") 
-print(y_mod_2_ln)
-print("and the error is: ")
-print(str(error_mod_2_ln) + " with standard error " + str(sse_2_ln))
-print("the standard deviation is :" + str(std(sse_2_ln, c+1)))
-
+print("It can be observed that the first model describes the data more"
+      " accurate than model 2. ")
+print ("This can be seen by comparing the sum of the"
+      " the squared errors and comparing the log likelihood of the two mode"
+      "ls. ")
 
 
 
